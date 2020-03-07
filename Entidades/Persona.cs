3a221b38b5577713bf887f2c9776acfcb914984b
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-03-06 20:59:08 -0400

    Se creo la clase Persona con todos sus atributos

--- Entidades/Persona.cs
+++ Entidades/Persona.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Personas.Entidades
 {
     public class Persona
     {
+        [Key]
         public int PersonaId { get; set; }
         public string Nombre { get; set; }
 
