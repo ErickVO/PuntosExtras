ef18d3deaf1f6cf28d3139ba5f0cab8c02f83c6f
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-03-06 20:46:21 -0400

    Se creo la  clase personas con todos sus atributos y su constructor

--- Entidades/Personas.cs
+++ Entidades/Personas.cs
@@ -6,6 +6,13 @@ namespace Personas.Entidades
 {
     public class Personas
     {
+        public int PersonaId { get; set; }
+        public string Nombre { get; set; }
 
+        public Personas()
+        {
+            PersonaId = 0;
+            Nombre = string.Empty;
+        }
     }
 }
